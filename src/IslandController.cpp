8ef4fcf104f5775982249e979307d2bf8b33c1d4
Mustafa <s235092@dtu.dk>
2024-12-05 20:42:33 +0100

    Creating an island controller

---
+++ src/IslandController.cpp
@@ -0,0 +1,3 @@
+#include "IslandController.h"
+
+IslandController::IslandController() = default;
---
+++ src/IslandController.h
@@ -0,0 +1,15 @@
+#ifndef ISLANDCONTROLLER_H
+#define ISLANDCONTROLLER_H
+
+
+
+class IslandController {
+public:
+    IslandController();
+private:
+    bool is_connected;
+};
+
+
+
+#endif //ISLANDCONTROLLER_H
