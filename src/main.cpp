bb7239ebf27d8b9546ec872683e6037201093695
EAL <elias@takefive.dk>
2024-12-19 23:29:23 +0100

    Refactored EngineBase creation to main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
-    IslandFactory();
+    auto gameEngineBase = std::make_shared<EngineBase>();
+    auto factoryGame = IslandFactory(gameEngineBase);
+    gameEngineBase->launch();
     return 0;
 }
