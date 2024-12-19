905a940e32a238b6bbdee0e60da9af6d385fa948
EAL <elias@takefive.dk>
2024-12-20 00:16:53 +0100

    Added explicit

--- src/Controllers/InventoryController.h
+++ src/Controllers/InventoryController.h
@@ -14,7 +14,7 @@
 class InventoryController
 {
 public:
-    InventoryController(std::shared_ptr<EngineBase>& engineBase);
+    explicit InventoryController(std::shared_ptr<EngineBase>& engineBase);
     std::shared_ptr<PlaceAble>& getSelectedPlaceAble();
 private:
     std::vector<std::shared_ptr<PlaceAble>> placeAbles_;
