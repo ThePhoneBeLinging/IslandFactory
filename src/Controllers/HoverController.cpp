e2e5f7cfc320564c2df8bdeb4509935be13c7067
EAL <elias@takefive.dk>
2024-12-20 13:03:06 +0100

    Offset is now seemingly handled properly

--- src/Controllers/HoverController.cpp
+++ src/Controllers/HoverController.cpp
@@ -13,8 +13,17 @@ void HoverController::handleHovering(const std::pair<int, int>& mousePos, const 
         return;
     }
     // TODO Take currentOffset into account
-    int modX = mousePos.first - (mousePos.first % Tile::TILESIZE);
-    int modY = mousePos.second - (mousePos.second % Tile::TILESIZE);
+
+    int mousePosXWithOffset = mousePos.first - static_cast<int>(currentOffset.first) % 50;
+    int mPosX = mousePosXWithOffset - mousePosXWithOffset % Tile::TILESIZE;
+    int modX = mPosX + static_cast<int>(currentOffset.first) % Tile::TILESIZE;
+
+
+    int mousePosYWithOffset = mousePos.second - static_cast<int>(currentOffset.second) % 50;
+    int mPosY = mousePosYWithOffset - mousePosYWithOffset % Tile::TILESIZE;
+    int modY = mPosY + static_cast<int>(currentOffset.second) % Tile::TILESIZE;
+
+    std::cout << mousePos.first << " " << mousePos.second << std::endl;
 
     placeAble->getTileDrawAble()->setPosition(modX,modY);
 }
