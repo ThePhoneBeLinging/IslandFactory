dad4a3a1eec3cbd48cd89a8e1d32865b23fe39da
Mustafa <s235092@dtu.dk>
2024-12-26 21:23:07 +0100

    Initial commit, working on factories

---
+++ src/Objects/Factories/Factory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by magnu on 26-12-2024.
+//
+
+#include "Factory.h"
---
+++ src/Objects/Factories/Factory.h
@@ -0,0 +1,16 @@
+//
+// Created by magnu on 26-12-2024.
+//
+
+#ifndef FACTORY_H
+#define FACTORY_H
+
+
+
+class Factory {
+
+};
+
+
+
+#endif //FACTORY_H
